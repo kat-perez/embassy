13afaa51382893f32faa869c364e0e57337053e2
Kat Perez <katperez@microsoft.com>
2025-03-06 11:25:16 -0500

    enable embassy-executor trace feature

--- examples/stm32f4/src/bin/blinky.rs
+++ examples/stm32f4/src/bin/blinky.rs
@@ -7,20 +7,27 @@ use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+#[embassy_executor::task]
+async fn run() {
+    info!("tick");
+    Timer::after_millis(300).await;
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+    spawner.spawn(run()).unwrap();
+    //let mut led = Output::new(p.PB7, Level::High, Speed::Low);
 
-    loop {
-        info!("high");
-        led.set_high();
-        Timer::after_millis(300).await;
+    //loop {
+    //    info!("high");
+    //    led.set_high();
+    //    Timer::after_millis(300).await;
 
-        info!("low");
-        led.set_low();
-        Timer::after_millis(300).await;
-    }
+    //    info!("low");
+    //    led.set_low();
+    //    Timer::after_millis(300).await;
+    //}
 }
