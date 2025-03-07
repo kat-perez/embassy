7a35b945fd27c46df367c7aec88ebfd530b9fa05
Kat Perez <katperez@microsoft.com>
2025-03-07 15:59:41 -0500

    WIP

--- examples/stm32h7/src/bin/blinky.rs
+++ examples/stm32h7/src/bin/blinky.rs
@@ -1,15 +1,19 @@
 #![no_std]
 #![no_main]
 
+use core::mem::MaybeUninit;
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::SharedData;
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+static SHARED_DATA: MaybeUninit<SharedData> = MaybeUninit::uninit();
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let p = embassy_stm32::init_primary(Default::default(), &SHARED_DATA);
     info!("Hello World!");
 
     let mut led = Output::new(p.PB14, Level::High, Speed::Low);
