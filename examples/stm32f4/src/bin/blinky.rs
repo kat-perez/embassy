c0b1a15ae50ef7c7ae4983fa01ffcfd0e2f650ec
Kat Perez <katperez@microsoft.com>
2025-03-07 12:00:19 -0500

    WIP

--- examples/stm32f4/src/bin/blinky.rs
+++ examples/stm32f4/src/bin/blinky.rs
@@ -1,24 +1,33 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+//use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
+use embassy_time::{Instant, Timer};
+//use {defmt_rtt as _, panic_probe as _};
+//use rtos_trace;
+use systemview_target::SystemView;
+use log::*;
+use panic_probe as _;
+
+static LOGGER: systemview_target::SystemView = systemview_target::SystemView::new();
+//rtos_trace::global_trace! {SystemView}
 
 #[embassy_executor::task]
 async fn run() {
-    info!("tick");
-    Timer::after_millis(300).await;
+    loop {
+        log::info!("tick");
+        Timer::after_millis(300).await;
+    }
 }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    LOGGER.init();
 
-    spawner.spawn(run()).unwrap();
+    //spawner.spawn(run()).unwrap();
     //let mut led = Output::new(p.PB7, Level::High, Speed::Low);
 
     //loop {
@@ -31,3 +40,20 @@ async fn main(spawner: Spawner) {
     //    Timer::after_millis(300).await;
     //}
 }
+
+#[unsafe(no_mangle)]
+unsafe extern "Rust" fn _embassy_trace_task_new(executor: u32, task: u32) {
+    log::info!("new task: {} {} {}", executor, task, Instant::now().as_ticks());
+}
+
+#[unsafe(no_mangle)]
+unsafe extern "Rust" fn _embassy_trace_task_exec_begin(executor: u32, task: u32) {}
+
+#[unsafe(no_mangle)]
+unsafe extern "Rust" fn _embassy_trace_task_exec_end(executor: u32, task: u32) {}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _embassy_trace_executor_idle(executor: u32) {}
+
+#[unsafe(no_mangle)]
+unsafe extern "C" fn _embassy_trace_task_ready_begin(executor: u32, task: u32) {}
